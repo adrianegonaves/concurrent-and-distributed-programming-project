91f61b2dbc80d0ce1228771f87881643715915a6
Adriane Almeida <you@example.com>
2026-05-17 14:30:13 +0100

    fix: ajuste e formatacao

--- main.go
+++ main.go
@@ -9,17 +9,15 @@ import (
 	"github.com/disintegration/imaging"
 )
 
-// o struct em minusculo indica é privado, ou seja, não pode ser acessado de fora do pacote
-// Exemplo de como calcular o destino dinamicamente:
-
-
+// notas sobre a sintaxe: o struct em minusculo indica é privado, ou seja, não pode ser acessado de fora do pacote.
+// criamos a struct Job para representar o trabalho que cada worker vai processar.
 type Job struct {
-  ImageURLOrigen string `json:"image_url"`
-  ImageURLDestino string `json:"image_url_destino"`
+  ImageURLOrigin string `json:"image_url"`
+  ImageURLDestination string `json:"image_url_destino"`
 }
 
-// nossa função worker vai receber o job como parametro
-func worker(id int, ch <-chan Job) {
+// nossa função worker vai receber o job como parametro ( consumidor) e vai consumir o job.
+func worker( ch <-chan Job) {
     for job := range ch {
         // chamamos a função no worker para ela pegar um job
         processImage(job)
@@ -27,19 +25,20 @@ func worker(id int, ch <-chan Job) {
     
 }
 
-// essa função funciona como o produtor 
-func pathToFile(sourceDir string, targetDir string, ch chan<- Job) {
-    err := filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
+// essa função funciona como o produtor ( recebe diretorio de origem e destino e o canal para enviar os jobs) e vai percorrer o diretório de origem, encontrar as imagens e enviar os jobs para os workers processarem.
+func pathToFile(sourceDirectory string, targetDirectory string, ch chan<- Job) {
+    err := filepath.Walk(sourceDirectory, func(path string, info os.FileInfo, err error) error {
         if err != nil {
             return err
         }
     
     if !info.IsDir() {
-        rel, _ := filepath.Rel(sourceDir, path)
-        novoDestino := filepath.Join(targetDir, rel)
+        // calculamos o caminho da imagens
+        rel, _ := filepath.Rel(sourceDirectory, path)
+        newDestination := filepath.Join(targetDirectory, rel)
         Job := Job{
-            ImageURLOrigen: rel,
-            ImageURLDestino: novoDestino,
+            ImageURLOrigin: path,
+            ImageURLDestination: newDestination,
             
         }
         // vamos enviar o job no canal ( recebemos o canal como parametro da função)
@@ -47,46 +46,54 @@ func pathToFile(sourceDir string, targetDir string, ch chan<- Job) {
     }
     return nil
     })
+
+    if err != nil {
+        log.Printf("Erro ao percorrer o diretório %s: %v", sourceDirectory, err)
+    }
 }
 
 //  criamos a função processImage para processar as imagens para ficar com o tamanho 48x48 e na escala cinza
 func processImage(job Job) {
-    image, err := imaging.Open(job.ImageURLOrigen)
+    image, err := imaging.Open(job.ImageURLOrigin)
     if err != nil {
-        log.Printf("Erro ao abrir a imagem %s: %v", job.ImageURLOrigen, err)
+        log.Printf("Erro ao abrir a imagem %s: %v", job.ImageURLOrigin, err)
         return 
     }
-    imageRedimensiona := imaging.Resize(image, 48, 48, imaging.Lanczos)
-    imageRedimensiona = imaging.Grayscale(imageRedimensiona)
-    os.MkdirAll(filepath.Dir(job.ImageURLDestino), os.ModePerm)
-    err = imaging.Save(imageRedimensiona, job.ImageURLDestino)
+    ResizeImage := imaging.Resize(image, 48, 48, imaging.Lanczos)
+    ResizeImage = imaging.Grayscale(ResizeImage)
+
+    os.MkdirAll(filepath.Dir(job.ImageURLDestination), os.ModePerm)
+
+    err = imaging.Save(ResizeImage, job.ImageURLDestination)
     if err != nil {
-        log.Printf("Erro ao salvar a imagem %s: %v", job.ImageURLDestino, err)
+        log.Printf("Erro ao salvar a imagem %s: %v", job.ImageURLDestination, err)
     }
 }
 
 
 func main() {
-    // o canal é criado para enviar e receber mensagens do tipo Job
-    image := make(chan Job)
-    
+    // o canal é criado para enviar e receber mensagens do tipo Job ( Job é a struct que criamos)
+    ch := make(chan Job)
     var wg sync.WaitGroup
 
     for i := 1; i <= 5; i++ {
         wg.Add(1)
-        // incia a goroutine para cada worker
         go func(id int) {
             defer wg.Done()
-            worker(id, image)
+            worker(ch)
         }(i)
     }
 
-    // funciona como um produtor para enviar o job
-    pathToFile('origem', 'destino', image)
+    // funciona como um produtor para enviar o job, essa função ocorre em segundo plano, ou seja, a função main não espera ela terminar para continuar a execução do código. Isso para não trovar a main.
+    go  func(){
+        pathToFile("origem", "destino", ch)
+        close(ch)
+    }() // não esquecer de colocar os parenteses para chamar a função anônima, caso contrário ela não será executada.
 
     // fechamos o canal quando os workers terminarem de processar os jobs, para evitar deadlocks
-    close(image)
+    
 
     wg.Wait()
+    log.Println("Processamento concluído com sucesso!")
 
 }
